fb133592e772f8999b1ad3ceb9fb74ad2d76b97d
Marcos <mnlmutan@gmail.com>
2026-02-19 22:01:10 -0300

    practicando con objetos

--- programa 23 diccionarios.py
+++ programa 23 diccionarios.py
@@ -2,12 +2,44 @@ cantidad=int(input("ingrese cantidad de personas"))
 
 personas=[]
 
+
+
+""" def buscar(dni):
+
+    for usuarios in personas:
+        if usuarios["documento"]==dni:
+            print("se ha encontrado que ",usuarios["nombre"],"coincide con el dni")
+        else:
+            print("no se encontro ningun usuario con ese dni") esta linea da multiples repeticiones por lo tanto
+             se realiza con una bandera """
+
+def buscar(dni):
+    encontrado = False
+    
+    for usuarios in personas:
+        if usuarios["documento"] == dni:
+            print("Se ha encontrado que", usuarios["nombre"], "coincide con el dni")
+            encontrado = True
+            break
+    
+    if not encontrado:
+        print("No se encontró ningún usuario con ese dni")
+
+def imprimir(arregloDePersonas):
+    print("estas son todas las personas del arreglo")
+    for items in arregloDePersonas:
+        print(items["nombre"])
+        print(items["documento"])
+
 for i in range(cantidad):
     objeto={
         "nombre":input("ingrese nombre"),
-        "edad":int(input("ingrese edad"))
+        "documento":int(input("ingrese dni"))
     }
     personas.append(objeto)
 
-for elementos in personas:
-    print(elementos["nombre"])
+buscando=int(input("ingrese dni de la persona que desea encontrar"))
+
+imprimir(personas)
+
+buscar(buscando)
